554f15d336a33af4ce25e17163c1a4306861384e
Muskan Agarwal <xiig27.muskanagarwal@gmail.com>
2024-09-04 01:42:43 +0530

    Create index.tsx

---
+++ data/src/api/index.tsx
@@ -0,0 +1,10 @@
+import FileUploadForm from '../components/fileuploadform';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Upload Your Files</h1>
+      <FileUploadForm />
+    </div>
+  );
+}
