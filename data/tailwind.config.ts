5c49cb049eb230eaa933270801c2ec8e6d986c1e
heres-aayush <maanaayush2003@gmail.com>
2024-09-06 23:35:01 +0530

    23:17 push... after how to use page again

--- data/tailwind.config.ts
+++ data/tailwind.config.ts
@@ -88,6 +88,7 @@ const config = {
   },
   plugins: [require("tailwindcss-animate"),addVariablesForColors],
 } satisfies Config
+
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
